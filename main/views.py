83e95499423b8bbc1bbc909f4391aba4a6dc196b
M H <morganhartley@gmail.com>
2017-11-06 13:56:21 -0600

    final push for client show

--- main/views.py
+++ main/views.py
@@ -18,23 +18,27 @@ def legislatorhandle(request):
 		georesponse = geo_api(location)
 		if georesponse['status'] =='OK':
 			return showleg(request, georesponse['lat'], georesponse['lng'])
+		else:
+			return render(request, 'home.html', {'error':True})
 	else:
 		return render(request, 'home.html', {'error':True})
-
 
 def showleg(request, lat, lng):
 	print(lat)
 	print(lng)
-	user_pt = geos.Point(lng, lat, srid=4326)
-	sd = District.objects.filter(chamber='s').get(geom__intersects=user_pt)
-	hd = District.objects.filter(chamber='h').get(geom__intersects=user_pt)
-	cd = District.objects.filter(chamber='c').get(geom__intersects=user_pt)
-	houserep = hd.incumbent
-	senrep = sd.incumbent
-	congrep = cd.incumbent
-	print('###$$$$$' + congrep.lastname)
-	string = 'HouseDistrict: %s controlled %s, SenateDistrict %s controlled %s in %s ' % (hd.district, hd.party, sd.district, sd.party, hd.city)
-	return render(request, 'showleg.html', {'cd': cd, 'sd': sd, 'hd': hd, 'congrep': congrep, 'houserep': houserep, 'senrep':senrep, 'lat':lat, 'lng':lng})
+	try:
+		user_pt = geos.Point(lng, lat, srid=4326)
+		sd = District.objects.filter(chamber='s').get(geom__intersects=user_pt)
+		hd = District.objects.filter(chamber='h').get(geom__intersects=user_pt)
+		cd = District.objects.filter(chamber='c').get(geom__intersects=user_pt)
+		houserep = hd.incumbent
+		senrep = sd.incumbent
+		congrep = cd.incumbent
+		print('###$$$$$' + congrep.lastname)
+		string = 'HouseDistrict: %s controlled %s, SenateDistrict %s controlled %s in %s ' % (hd.district, hd.party, sd.district, sd.party, hd.city)
+		return render(request, 'showleg.html', {'cd': cd, 'sd': sd, 'hd': hd, 'congrep': congrep, 'houserep': houserep, 'senrep':senrep, 'lat':lat, 'lng':lng})
+	except:
+		return error(request)
 
 def vote_table(request, incumbent_id):
 	pol = Politician.objects.get(id=incumbent_id)
@@ -60,6 +64,9 @@ def emailinputhandle(request):
 				i.save()
 			except:
 				transaction.rollback()
+
+def error(request):
+	return render(request, 'error.html')
 
 
 
